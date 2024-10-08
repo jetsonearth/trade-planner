a2fdafd3782abc5a7a4d03cc86d9fcf173f12a60
Zhaobang Wu <zhaobang.jet.wu@gmail.com>
2024-10-08 23:23:33 +0800

    Initial commit

---
+++ trade-planner/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TradePlanner from './TradePlanner';
+
+function App() {
+  return (
+    <div className="App">
+      <TradePlanner />
+    </div>
+  );
+}
+
+export default App;
